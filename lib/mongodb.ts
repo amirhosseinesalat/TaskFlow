a94ce1ce520f9988bd7f5e4f596e33c52293075b
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-08-20 20:33:52 +0430

    complete routes file

---
+++ lib/mongodb.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI: string | undefined | any = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined");
+}
+
+export async function connectDB() {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  await mongoose.connect(MONGODB_URI);
+  console.log("MongoDB connected");
+}
